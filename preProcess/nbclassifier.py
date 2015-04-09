5e8c4e2c74958aaeb17e627c1d210f7428cfe860
blood8088 <blood8088@gmail.com>
2015-04-09 17:53:32 -0500

    Replace for loop with mpmath.fprod

--- preProcess/nbclassifier.py
+++ preProcess/nbclassifier.py
@@ -1,37 +1,29 @@
 class NaiveBayesianClassifier:
 
     def __init__(self,xTr,yTr):
-        from numpy import array,where
+        from numpy import array,where,amin
         self.n = yTr.shape[0]
         self.pis = ((array(xTr.sum(0))[0])+.5)/(self.n+1.0)
         self.labelset = [lbl for lbl in set(yTr) if lbl!='']
         self.indexset = [where(yTr==lbl)[0] for lbl in self.labelset]
-        self.likelihood = [(xTr[index].getnnz(0)+self.pis)/(len(index)+1) for index in self.indexset]
-        
+        self.likelihood = array([(xTr[index].getnnz(0)+self.pis)/(len(index)+1) for index in self.indexset])
+
     def predict(self,xTe):
-        from numpy import array,float64
+        from numpy import array,argmax
+        from sympy.mpmath import fprod
         nPr = xTe.shape[0]
         yPr = [""]*xTe.shape[0]
         print('{}{}'.format("Number of classes: ",len(self.likelihood)))
         for i in range(nPr):
             x = xTe.getrow(i)
-            maxVal = float64(0.0)
-            maxIndex = -1
-            for j in range(len(self.likelihood)):
-                posterior = float64(1.0)
-                for pvg in self.likelihood[j][x.indices]:
-                    posterior*=float64(pvg)
-                if posterior>maxVal:
-                    maxVal = posterior
-                    maxIndex = j
-            if maxIndex>-1:
-                yPr[i]=self.labelset[maxIndex]
-            else:
-                yPr[i]=''
+            likeli = self.likelihood[:,x.indices]          
+            posterior = fprod(likeli.transpose())
+            maxIndex = argmax(posterior)
+            yPr[i]=self.labelset[maxIndex]
         return array(yPr)
         
     def accuracy(self,yTe,yPr):
-        from numpy import float64
+        from numpy import float32
         if yTe.shape[0]==yPr.shape[0]:
             matchCount = 0.0
             totalCount = 0.0
@@ -40,4 +32,4 @@ class NaiveBayesianClassifier:
                     if yTe[i] == yPr[i]:
                         matchCount+=1.0
                     totalCount+=1.0
-            return float64(matchCount)/totalCount
+            return float32(matchCount)/totalCount
--- preProcess/nbfrun.py
+++ preProcess/nbfrun.py
@@ -1,16 +1,13 @@
-import util,numpy,scipy
+import util,numpy
 from util import load_feature_from_npz,load_label_from_npz
 from labelParser import LabelParser
 import nbclassifier,slicer
-import multiprocessing as mp
 
-def nbclassify(iteration=1,lessData=False,test=False):
+def nbclassify(iteration=1,lessData=False,test=True):
     print("Loading features and labels...")
     features = load_feature_from_npz(util.dataPath+'features.npz')
     labels = load_label_from_npz(util.dataPath+'labels.npz')
     print("Loading finished.")
-    print('{}{}'.format('features.shape = ',features.shape))
-    print('{}{}'.format('labels.shape = ',labels.shape))
 
     if lessData:
         portion = int(features.shape[0]/5)+1
@@ -24,15 +21,13 @@ def nbclassify(iteration=1,lessData=False,test=False):
     print("Begin cutting data into training dataset and testing dataset...")
     xTr,yTr,xTe,yTe = slr.slice(features,labels)
     print("Cutting finished.")
+    
+    del features,labels
 
     if test:
-        maxIter=2
+        maxIter=1
     else:
         maxIter=LabelParser.maxTax
-
-    yPr = numpy.full(yTe.shape,'',dtype=str)
-    print(yPr)
-    print(yPr.shape)
 
     for i in range(maxIter):
         print('{}{}'.format(i,"th taxonomy classification:"))
@@ -42,8 +37,6 @@ def nbclassify(iteration=1,lessData=False,test=False):
         print("Starting Testing...")
         prediction = nbc.predict(xTe)
         print(prediction)
-        for j in range(len(prediction)):
-            yPr[j,i] = prediction[j]
         print("Testing finished.")
         print("Starting calculating accuracy...")
         acc[i] += nbc.accuracy(yTe[:,i],prediction)
@@ -53,10 +46,8 @@ def nbclassify(iteration=1,lessData=False,test=False):
     for i in range(LabelParser.maxTax):
         accDict[LabelParser.taxDict[i]] = acc[i]
     
-    print(accDict)
-    print(yPr)    
+    print(accDict)    
     numpy.savez(util.dataPath+"accuracy",accuracy = accDict)
-    #numpy.savetxt('{}{}{}'.format(util.dataPath+'yPr_',iteration,'.txt'),yPr)
 
 if __name__ == "__main__":
     import sys
@@ -66,4 +57,5 @@ if __name__ == "__main__":
         nbclassify(iteration=int(sys.argv[1]))
     elif len(sys.argv)==3:
         nbclassify(iteration=int(sys.argv[1]),lessData = (sys.argv[2]=='lessData'))
-
+    elif len(sys.argv)==4:
+        nbclassify(iteration=int(sys.argv[1]),lessData = (sys.argv[2]=='lessData'),test = (sys.argv[3]=='test'))
